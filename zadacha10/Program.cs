4f49002de47701d929ab29f79c52133346f5b7b7
R.Minachev <vardol@mail.ru>
2022-06-29 21:11:06 +0300

    homework second lesson

---
+++ zadacha10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = 0;
+while (number < 100)
+{
+    Console.Write("Введите трехзначное число - ");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number < 0)
+    {
+        number = -number;
+    }
+    if (number < 100 || number >= 1000)
+    {
+        Console.WriteLine("Введено не трехзначное число!");
+    }
+}
+number = number / 10;
+Console.WriteLine(number % 10);
---
+++ zadacha15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int number = 0;
+while (number < 1 || number > 7)
+{
+    Console.Write("Введите цифру обозначающую день недели - ");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number < 1 || number > 7)
+    {
+        Console.WriteLine("Нет такого дня недели!");
+    }
+}
+
+if (number > 5)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
