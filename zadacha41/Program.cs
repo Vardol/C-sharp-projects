0e55601e7556ced44d7861566f4c262c28163dd7
R.Minachev <vardol@mail.ru>
2022-07-15 19:46:31 +0300

    homework for lesson 6

---
+++ zadacha41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int ReadNaturalInt(string promtingMessageForUser)
+{
+    int result = -1;
+    while (result <= 0)
+    {
+        Console.Write(promtingMessageForUser);
+        result = Convert.ToInt32(Console.ReadLine());
+        if (result <= 0)
+        {
+            Console.WriteLine("введено ненатуральное число");
+        }
+    }
+    return result;
+}
+
+int countOfNumbers = ReadNaturalInt("Сколько чисел будем вводить? ");
+int countOfPositiveNumbers = 0;
+double currentNumber = -1;
+
+for (int i = 1; i <= countOfNumbers; i++)
+{
+    Console.Write("Введите " + i + " число - ");
+    currentNumber = Convert.ToDouble(Console.ReadLine());
+    if (currentNumber > 0) //будем считать, что ноль - это неположительное число
+    {
+        countOfPositiveNumbers++;
+    }
+}
+
+Console.WriteLine("Количество положительных чисел среди введенных - " + countOfPositiveNumbers);
---
+++ zadacha43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Программа ищет точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1; y = k2 * x + b2.");
+Console.Write("введите значение k1 - ");
+double numberK1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите значение b1 - ");
+double numberB1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите значение k2 - ");
+double numberK2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите значение b2 - ");
+double numberB2 = Convert.ToDouble(Console.ReadLine());
+
+//преобразование уравнений для расчета х
+// k1*x+b1=k2*x+b2
+// k1*x-k2*x=b2-b1
+// x*(k1-k2)=b2-b1
+// x=(b2-b1)/(k1-k2)
+
+//преобразование уравнений для расчета y
+// (y-b1)/k1=(y-b2)/k2
+// (k2(y-b1)+b2*k1)/k1=y
+// b2*k1-k2*b1=k1*y-k2*y
+// y=(b2*k1-k2*b1)/(k1-k2)
+
+Console.WriteLine("Точка пересечения прямых находится в координатах: " + ((numberB2-numberB1)/(numberK1-numberK2)) + ", " + ((numberB2*numberK1-numberK2*numberB1)/(numberK1-numberK2)) + ".");
