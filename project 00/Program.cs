c216561c73ed9dc3b1040252a4c84b2e05776ebc
R.Minachev <vardol@mail.ru>
2022-07-17 23:46:54 +0300

    figured out int values of certain characters

--- project 00/Program.cs
+++ project 00/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hi, All!");
+﻿// Console.WriteLine("Hi, All!");
+
+string s = "-0123456789,";
+int characterZero = s[1];
+int characterNine = s[10];
+int characterMinus = s[0];
+int characterComma = s[11];
+Console.WriteLine("int of '0' character = " + characterZero);
+Console.WriteLine("int of '9' character = " + characterNine);
+Console.WriteLine("int of '-' character = " + characterMinus);
+Console.WriteLine("int of ',' character = " + characterComma);
